74c0bf1091d367b85fefe306452afdbc345eeb5c
Patrick Landin <patricklandin@gmail.com>
2014-12-27 00:06:24 -0800

    Added startNewRound function.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -15,39 +15,47 @@ class ViewController: UIViewController {
 
 	@IBAction func showAlert(sender: AnyObject) {
 			
-			let sliderMessage = "The value of the slider is: \(currentValue)"
+		let sliderMessage = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
         
-			let alert = UIAlertController(title: "Helllooorr", message: sliderMessage, preferredStyle: .Alert)
+		let alert = UIAlertController(title: "Helllooorr", message: sliderMessage, preferredStyle: .Alert)
         
-			let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+		let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
         
-			alert.addAction(action)
+		alert.addAction(action)
         
-			presentViewController(alert, animated: true, completion: nil)
+		presentViewController(alert, animated: true, completion: nil)
+		
+		self.startNewRound()
 			
-    }
+	}
 	
     
 	@IBAction func sliderMoved(slider: UISlider) {
         
-			currentValue = lroundf(slider.value)
+		currentValue = lroundf(slider.value)
         
-    }
+	}
 	
+	func startNewRound() {
+		
+		targetValue = 1 + Int(arc4random_uniform(100))
+		currentValue = 50
+		slider.value = Float(currentValue)
+		
+	}
     
-    override func viewDidLoad() {
-			super.viewDidLoad()
+	override func viewDidLoad() {
+		super.viewDidLoad()
 			
-			currentValue = lroundf(slider.value)
-			targetValue = 1 + Int(arc4random_uniform(100))
+		self.startNewRound()
 			
-		}
+	}
 	
 
-    override func didReceiveMemoryWarning() {
-			super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 
 
 }
