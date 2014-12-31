c08682f880aec399c84252f203e8bcbe3049b90c
Patrick Landin <patricklandin@gmail.com>
2014-12-30 17:00:47 -0800

    Added difference variable and method.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -7,16 +7,28 @@ import UIKit
 class ViewController: UIViewController {
     
 	var currentValue : Int = 0
-	
 	var targetValue : Int = 0
+	var difference : Int = 0
 	
-    
+	
 	@IBOutlet weak var slider: UISlider!
 	@IBOutlet weak var targetLabel: UILabel!
 	
 	@IBAction func showAlert(sender: AnyObject) {
+		
+
+		if currentValue > targetValue {
+			difference = currentValue - targetValue
 			
-		let sliderMessage = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
+		} else if targetValue > currentValue {
+			difference = targetValue - currentValue
+			
+		} else {
+			difference = 0
+		}
+		
+			
+		let sliderMessage = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)" + "\nThe difference is: \(difference)"
         
 		let alert = UIAlertController(title: "Helllooorr", message: sliderMessage, preferredStyle: .Alert)
         
@@ -28,36 +40,34 @@ class ViewController: UIViewController {
 		
 		self.startNewRound()
 		self.updateLabels()
-			
 	}
 	
     
 	@IBAction func sliderMoved(slider: UISlider) {
         
 		currentValue = lroundf(slider.value)
-        
 	}
+	
 	
 	func startNewRound() {
 		
 		targetValue = 1 + Int(arc4random_uniform(100))
 		currentValue = 50
 		slider.value = Float(currentValue)
-		
 	}
+	
 	
 	func updateLabels() {
 		
 		targetLabel.text = String(targetValue)
-		
 	}
-    
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 			
 		self.startNewRound()
 		self.updateLabels()
-			
 	}
 	
 
