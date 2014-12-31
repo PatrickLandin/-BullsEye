2d756afe25f99afb7addc117f489ab334a6606cb
Patrick Landin <patricklandin@gmail.com>
2014-12-30 21:15:19 -0800

    Fully functioning. Info light working. Images and styling added.

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,34 @@
+
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+	@IBAction func closeAbout(sender: AnyObject) {
+		
+		dismissViewControllerAnimated(true, completion: nil)
+	}
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
